4d183b65ffce90c872a9b26657db886da570f2b9
David Acosta <davidacosta25081@yahoo.com>
2019-02-03 19:11:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,8 @@
 def my_each(collection)
   i = 0
   while i < collection.length
-    yield collection[i]
-  
+    yield (collection[i])
+  i +=1
   end
-end
-
-
-
-my_each(collection) do |i|
-  puts i
+collection
 end
